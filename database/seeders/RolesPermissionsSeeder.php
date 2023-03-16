e7de7dd2ed5d180968574abcdd1ec5333b604250
danhnq <danhkhh@gmail.com>
2023-03-16 18:14:39 +0700

    Install spatie/laravel-permission + Seed data

---
+++ database/seeders/RolesPermissionsSeeder.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Guard;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RolesPermissionsSeeder extends Seeder
+{
+    /**
+     * @var array
+     */
+    const ALL_PERMISSIONS = [
+        'loan.create',
+        'loan.update',
+        'loan.delete',
+        'loan.view',
+        'list loan',
+
+        'repayment.create',
+        'repayment.update',
+        'repayment.delete',
+        'repayment.view',
+        'list repayment',
+        'makePayment',
+    ];
+
+    /**
+     * @var array
+     */
+    const ADMIN_PERMISSIONS = [
+        'loan.create',
+        'loan.update',
+        'loan.delete',
+        'loan.view',
+        'list loan',
+
+        'repayment.create',
+        'repayment.update',
+        'repayment.delete',
+        'repayment.view',
+        'list repayment',
+        'makePayment',
+    ];
+
+    /**
+     * @var array
+     */
+    const CUSTOMER_PERMISSIONS = [
+        'loan.create',
+        'loan.view',
+        'list loan',
+        'repayment.create',
+        'repayment.view',
+        'list repayment',
+        'makePayment',
+    ];
+
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $this->createPermissions();
+        $this->createRoleAdmin();
+        $this->createRoleCustomer();
+    }
+
+    /**
+     * Create permissions
+     *
+     * @return void
+     */
+    private function createPermissions(): void
+    {
+        $permissions = collect(self::ALL_PERMISSIONS)->map(
+            function ($permission) {
+                return ['name' => $permission, 'guard_name' => Guard::getDefaultName(static::class)];
+            }
+        )->toArray();
+        Permission::insert($permissions);
+    }
+
+    /**
+     * Create admin role and assign permissions
+     *
+     * @return void
+     */
+    private function createRoleAdmin(): void
+    {
+        $role = Role::create(['name' => config('roles.ADMIN')]);
+        $role->givePermissionTo(self::ADMIN_PERMISSIONS);
+    }
+
+    /**
+     * Create customer role and assign permissions
+     *
+     * @return void
+     */
+    private function createRoleCustomer(): void
+    {
+        $role = Role::create(['name' => config('roles.CUSTOMER')]);
+        $role->givePermissionTo(self::CUSTOMER_PERMISSIONS);
+    }
+}
---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [
+            [
+                'role'              => config('.ADMIN'),
+                'name'              => 'admin',
+                'email'             => 'admin@test.com',
+                'password'          => bcrypt('111111'),
+                'email_verified_at' => Carbon::now(),
+            ],
+            [
+                'role'              => config('roles.CUSTOMER'),
+                'name'              => 'customer',
+                'email'             => 'customer@test.com',
+                'password'          => bcrypt('111111'),
+                'email_verified_at' => Carbon::now(),
+            ],
+        ];
+
+        foreach ($users as $user) {
+            if (User::whereEmail($user['email'])->count() === 0) {
+                $role = $user['role'];
+                unset($user['role']);
+                $record = User::factory()->create($user);
+                $record->assignRole($role);
+            } else {
+                echo "Skipping {$user['email']} as they already exist\n";
+            }
+        }
+    }
+}
